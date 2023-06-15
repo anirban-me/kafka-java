8116375bfd9c0513e9449fef95d543e919939831
Anirban Bhowmick <anirbanb1908@gmail.com>
2023-06-15 13:10:33 +0530

    Comment

--- kafka-connect/connectors/run.sh
+++ kafka-connect/connectors/run.sh
@@ -1,1 +1,2 @@
 connect-standalone /Users/anirbanbhowmick/Documents/repos/kafka-java/kafka-connect/connect-standalone.properties /Users/anirbanbhowmick/Documents/repos/kafka-java/kafka-connect/twitter.properties
+# change path as per requirement
